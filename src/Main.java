9c0b690198a86a8b0d665df8f3f28d2725f5a3f6
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-04-22 19:19:33 +0600

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание №1
         boolean clientOS;
-        char clientOperSys = 'O'; //Значение 'А' - Android; 'i' - iOS.
-        if (clientOS = clientOperSys == 'A') {System.out.println("Download our application for Android here.");}
-        else if (clientOS = clientOperSys == 'i') {System.out.println("Download our application for iOS here.");}
-        else System.out.println("You use unknown operating system. Please contact to system administrator.");
-          }
+        char clientOperSys = 'i'; //Значение 'А' - Android; 'i' - iOS.
+        if (clientOS = clientOperSys == 'A') {
+            System.out.println("Download our application for Android here.");
+        } else {
+            System.out.println("Download our application for iOS here.");
+        }
+        System.out.println("-----------------------------------------");
+
+        //Задание №2
+        int clientDeviceYear = 2015;
+        if (clientOS && clientDeviceYear < 2015) {
+            System.out.println("We recommend you to install light version of our application for Android from here.");
+        }
+        if (clientOS && clientDeviceYear >= 2015) {
+            System.out.println("Congratulations! Your device will work properly with our newest application for Android! You may download it here.");
+        }
+        if (!clientOS && clientDeviceYear < 2015) {
+            System.out.println("We recommend you to install light version of our application for iOS from here.");
+        }
+        if (!clientOS && clientDeviceYear >= 2015){
+            System.out.println("Congratulations! Your device will work properly with our newest application for iOS! You may download it here.");
+        }
+        System.out.println("-----------------------------------------");
+    }
+//Задание №3
+
 }
+
+
