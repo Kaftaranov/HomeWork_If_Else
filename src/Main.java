fd72d732f90881e7a4431258a46474cc2903a4d5
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-04-22 18:33:19 +0600

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        boolean clientOS;
+        char clientOperSys = 'O'; //Значение 'А' - Android; 'i' - iOS.
+        if (clientOS = clientOperSys == 'A') {System.out.println("Download our application for Android here.");}
+        else if (clientOS = clientOperSys == 'i') {System.out.println("Download our application for iOS here.");}
+        else System.out.println("You use unknown operating system. Please contact to system administrator.");
+          }
 }
